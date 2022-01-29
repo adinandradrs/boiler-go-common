246768c18e6217e6fd78c6eb531ace9a811a4cb1
nandra <adinandra.dharmasurya@gmail.com>
2022-01-30 00:22:16 +0700

    update code

--- base/helper.go
+++ base/helper.go
@@ -1,25 +1,5 @@
 package base
 
-import (
-	"github.com/jackc/pgx/v4/pgxpool"
-)
-
-type BaseRepositoryCapsule struct {
-	Database *pgxpool.Pool
-}
-
-func BaseRepository(database *pgxpool.Pool) *BaseRepositoryCapsule {
-	return &BaseRepositoryCapsule{
-		Database: database,
-	}
-}
-
-type BaseRepositoryInterface interface {
-	Save(input interface{}) (interface{}, error)
-	FindById(id uint) (interface{}, error)
-	Delete(id uint) error
-}
-
-type BaseServiceInterface interface {
+type BaseService interface {
 	Execute(input interface{}) (interface{}, error)
 }
